bd66e811374e935804b17ea2b04132e5f2389963
cristianworth <cristianweissmantel@gmail.com>
2023-12-16 19:26:33 -0300

    Fixed bug for decode the token

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,9 +1,7 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import api from "./api";
 import { jwtDecode } from "jwt-decode";
-import jwt from "jsonwebtoken";
-import base64 from 'react-native-base64';
-import crypto from 'crypto'
+import "core-js/stable/atob";
 
 export async function doLogin(email: string, senha: string) {
   if (!email || !senha) return null;
@@ -25,16 +23,14 @@ export async function doLogin(email: string, senha: string) {
 }
 
 export function storeLoginData(loginData: any) {
-  var { token, userId } = loginData;
+  var { token } = loginData;
   
   AsyncStorage.setItem("token", token);
   console.log("Login token:", token);
-  AsyncStorage.setItem("pacienteId", userId);
-  console.log("Login pacienteId:", userId);
 
-  // TODO: The method decodeTokenId it's not working, so I can't get the pacienteId from the token
-  // const pacienteId = decodeTokenId(token);
-  // AsyncStorage.setItem("pacienteId", pacienteId);
+  const pacienteId = decodeTokenId(token);
+  AsyncStorage.setItem("pacienteId", pacienteId);
+  console.log("Login pacienteId:", pacienteId);
 }
 
 function decodeTokenId(token: string) {
@@ -44,35 +40,19 @@ function decodeTokenId(token: string) {
       role: string;
     }
   
-    // FIXME jsonwebtoken doesn't work, error log:
-    // The package at "node_modules\jsonwebtoken\verify.js" attempted to import the Node standard library module "crypto".
-    // It failed because the native React runtime does not include the Node standard library.
-    // const secretKey: string = process.env.SECRET || "qualquercoisa";
-    // const decodedToken: TokenProps = jwt.verify(
-    //   token,
-    //   secretKey
-    // ) as TokenProps;
-    // console.log("jsonwebtoken:", decodedToken);
-
-    // FIXME jwt-decode doesn't work, error log:
-    // Error decoding token: [InvalidTokenError: Invalid token specified: invalid base64 for part #2 (Property 'atob' doesn't exist)]
     const decodedToken = jwtDecode(token) as TokenProps;
     console.log("jwt-decode:", decodedToken);
 
     return decodedToken.id;
   } catch (error) {
     console.error("Error decoding token:", error);
+    // If fails return a default Id.
     const decodedId = "2579d15f-f4f1-4a09-80dc-9797bfff3887";
     return decodedId;
   }
 }
 
 export async function removeLoginDataFromStorage() {
-  const token = await AsyncStorage.getItem("token");
-  console.log("Logout token:", token);
-  const pacienteId = await AsyncStorage.getItem("pacienteId");
-  console.log("Logout pacienteId:", pacienteId);
-
   AsyncStorage.removeItem("token");
   AsyncStorage.removeItem("pacienteId");
 }
